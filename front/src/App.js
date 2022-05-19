5743352579f2f235446251d64253ace06058b194
Singery <singery00@naver.com>
2022-05-19 18:22:58 +0000

    Project

--- front/src/App.js
+++ front/src/App.js
@@ -4,8 +4,11 @@ import Login from "./component/Login"
 import Main from "./component/Main"
 import Register from "./component/Register"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import axios from 'axios';
+
 
 function App() {
+	
   return (
 		<div className = 'App'>
 		  <BrowserRouter>
