c727bd202b68944d6bcfb4eb690e8f3ca9498e43
Singery <singery00@naver.com>
2022-05-20 18:18:25 +0000

    db connect

--- front/server/config/db.js
+++ front/server/config/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 const db = mysql.createPool({
     host : 'localhost',
     user : 'root',
-    password : 'mysql',
+    password : 'mysql1234',
     database : 'db'
 });
 
--- front/server/server.js
+++ front/server/server.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 5000 // <- 3000에서 다른 숫자로 변경
+const cors = require("cors");
 const db = require('./config/db');
+const bodyParser = require("body-parser");
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "root", //mysql의 id
+    password : "mysql1234", //mysql의 password
+    database : "db", //사용할 데이터베이스
+});
+
+connection.connect();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
 	console.log("running")
   res.send('dddddd!')
 })
 
-app.get('/api/products', (req, res) => {
+app.get('/api/info', (req, res) => {
     db.query("SELECT * FROM game", (err, data) => {
         if(!err) res.send({ products : data });
         else res.send(err);
